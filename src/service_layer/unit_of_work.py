890152d2cff3cf371d0bb446f5a4d06cd0015b05
Krystian Jarmul <krystian.jarmul@ericsson.com>
2021-02-09 06:39:34 +0100

    Introduce Unit Of Work pattern

---
+++ src/service_layer/unit_of_work.py
@@ -0,0 +1,48 @@
+import abc
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from src import database
+from src.adapters import repository
+
+
+class AbstractUnitOfWork(abc.ABC):
+    jobs: repository.AbstractRepository
+
+    def __exit__(self, *args):
+        self.rollback()
+
+    @abc.abstractmethod
+    def commit(self):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def rollback(self):
+        raise NotImplementedError
+
+
+DEFAULT_SESSION_FACTORY = sessionmaker(
+    bind=create_engine('sqlite:///sqlite3.db')
+)
+
+
+class SqlAlchemyUnitOfWork(AbstractUnitOfWork):
+
+    def __init__(self, session_factory=DEFAULT_SESSION_FACTORY):
+        self.session_factory = session_factory
+
+    def __enter__(self):
+        self.session = self.session_factory()
+        self.jobs = repository.SqlAlchemyRepository(self.session)
+        return super().__enter__()
+
+    def __exit__(self, *args):
+        super().__exit__(*args)
+        self.session.close()
+
+    def commit(self):
+        self.session.commit()
+
+    def rollback(self):
+        self.session.rollback()
